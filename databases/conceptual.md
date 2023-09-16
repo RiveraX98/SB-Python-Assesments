cb12ca77cc06667672e6fac9ff0773bec525ba03
Xavier Rivera <xrivera@Xaviers-MacBook-Air.local>
2023-09-15 22:46:07 -0400

    conceptual.md added

--- databases/conceptual.md
+++ databases/conceptual.md
@@ -3,26 +3,45 @@
 Answer the following questions below:
 
 - What is PostgreSQL?
-  SQLAlchemy is a library that facilitates the communication between Python programs and databases
   PostgreSQL is a powerful, open source object-relational database system that uses and extends the SQL language
 
 - What is the difference between SQL and PostgreSQL?
+    SQL - Sructured query language
+        - A language to write querys to databases
+    PostgreSQL - A relational database management system that uses and extends the SQL language combined with other features  
 
 - In `psql`, how do you connect to a database?
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///{database_name}'
 
 - What is the difference between `HAVING` and `WHERE`?
+      Where - Filters results from the table
+      Having - Filters results from a group
 
 - What is the difference between an `INNER` and `OUTER` join?
+      Inner Join - returns matching data from 2+ tables based off specified criteria
+      Outer Join - An inner join but with addional data being returned by the table being specified, either right, left or full
 
 - What is the difference between a `LEFT OUTER` and `RIGHT OUTER` join?
+      Right Outer - returns all the records from the Right table
+      Left Outer - returns all the records from the Left table
 
 - What is an ORM? What do they do?
+      Object Relational Mapper -  technique for converting data between a relational database and object-oriented programming language
+     
+      SQLAlchemy is an ORM written in Python to give developers the power and flexibility of SQL
+          
 
 - What are some differences between making HTTP requests using AJAX 
   and from the server side using a library like `requests`?
+    HTTP request - client side - runs though the browser, 
+    Server side - request are processing on web servers
+
 
 - What is CSRF? What is the purpose of the CSRF token?
+    CSRF - Cross-Site Request Forgery -  an attack that forces authenticated users to submit a request to a Web application against which they are currently authenticated.
+    CSRF Token - CSRF Tokens are secure random tokens that are hidden in a users session and sent along with a request to verify legitimacy of the end-user request.
+               
   
 
 - What is the purpose of `form.hidden_tag()`?
-  To hide to csrf validation field from the UI
+  To hide to csrf token from the UI
--- databases/playlist-app/app.py
+++ databases/playlist-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template,flash
+from flask import Flask, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, Playlist, Song, PlaylistSong
@@ -13,7 +13,6 @@ app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "Keyabc123"
 
 
-
 connect_db(app)
 db.create_all()
 debug = DebugToolbarExtension(app)
@@ -25,12 +24,9 @@ debug = DebugToolbarExtension(app)
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 
-
-
 @app.route("/")
 def root():
     """Homepage: redirect to /playlists."""
-    
 
     return redirect("/playlists")
 
@@ -52,7 +48,7 @@ def show_playlist(playlist_id):
     """Show detail on specific playlist."""
     playlist = Playlist.query.get_or_404(playlist_id)
 
-    return render_template("playlist.html", playlist= playlist)
+    return render_template("playlist.html", playlist=playlist)
 
 
 @app.route("/playlists/add", methods=["GET", "POST"])
@@ -64,19 +60,17 @@ def add_playlist():
     """
 
     # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
-    form=PlaylistForm()
+    form = PlaylistForm()
     if form.validate_on_submit():
         name = form.name.data
-        description= form.description.data
-        new_playlist = Playlist(name=name, description= description)
+        description = form.description.data
+        new_playlist = Playlist(name=name, description=description)
         db.session.add(new_playlist)
         db.session.commit()
         flash("playlist added successfully", "success")
         return redirect("/playlists")
     else:
-        return render_template("new_playlist.html", form = form)
-
-
+        return render_template("new_playlist.html", form=form)
 
 
 ##############################################################################
@@ -95,7 +89,7 @@ def show_all_songs():
 def show_song(song_id):
     """return a specific song"""
     song = Song.query.get_or_404(song_id)
-    return render_template("song.html", song=song )
+    return render_template("song.html", song=song)
 
     # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
 
@@ -113,52 +107,61 @@ def add_song():
     if form.validate_on_submit():
         artist = form.artist.data
         title = form.title.data
-        new_song= Song(artist=artist, title=title)
+        new_song = Song(artist=artist, title=title)
         db.session.add(new_song)
         db.session.commit()
-        song= Song.query.filter_by(title=title, artist=artist).first()
+        song = Song.query.filter_by(title=title, artist=artist).first()
         flash("song added successfully", "success")
         return redirect(f"/songs/{song.id}")
     else:
         return render_template("new_song.html", form=form)
 
 
+# @app.route("/playlists/<int:playlist_id>/list_songs")
+# def show_playlist_page(playlist_id):
+#     """Add a playlist and redirect to list."""
+#     print('show list of songs')
+#     playlist = Playlist.query.get_or_404(playlist_id)
 
-@app.route("/playlists/<int:playlist_id>/add_song", methods=["GET", "POST"])
-def show_playlist_page(playlist_id):
-    """Add a playlist and redirect to list."""
+#     form = NewSongForPlaylistForm()
 
-  
+#     curr_on_playlist = [song.id for song in playlist.songs]
+#     choices = db.session.query(Song.title).filter(Song.id.notin_(curr_on_playlist)).all()
     
+#     form.song.choices = []
+#     for song in choices:
+#         form.song.choices.append(" ".join(song))
+        
+   
+
+#     return render_template("add_song_to_playlist.html", playlist=playlist, form=form, songs=form.song.choices)
+
+
+
+@app.route("/playlists/<int:playlist_id>/list_songs", methods=["GET","POST"])
+def add_song_to_playlist(playlist_id):
+    """Add a playlist and redirect to list."""
     playlist = Playlist.query.get_or_404(playlist_id)
 
     form = NewSongForPlaylistForm()
-    
 
     curr_on_playlist = [song.id for song in playlist.songs]
-    form.song.choices = db.session.query(Song.id, Song.title).filter(Song.id.notin_(curr_on_playlist)).all()
+    choices = db.session.query(Song.title).filter(Song.id.notin_(curr_on_playlist)).all()
+    
+    form.song.choices = []
+    for song in choices:
+        form.song.choices.append(" ".join(song))
+    
 
     if form.validate_on_submit():
-          song= form.song.data
-          new_playlistSong=PlaylistSong(playlist_id =None,song_id=song)
-          db.session.add(new_playlistSong)
-          db.session.commit()
-          flash("Song added to playlists", "success")
+        chosen_song = form.song.data
+        song= Song.query.filter_by(title=chosen_song).first()
 
-          return redirect(f"/playlists/{playlist_id}")
+        new_playlistSong = PlaylistSong(playlist_id=playlist_id, song_id=song.id)
+        db.session.add(new_playlistSong)
+        db.session.commit()
+        flash("Song added to playlists", "success")
 
-    return render_template("add_song_to_playlist.html", playlist=playlist,form=form)
+        return redirect(f"/playlists/{playlist_id}")
 
-
-
-# @app.route("/playlists/<int:playlist_id>/add-song", methods= ["POST"])
-# def add_song_to_playlist(playlist_id):
-#         form = NewSongForPlaylistForm()
-#         form.validate_on_submit()
-#         song= form.song.data
-#         new_playlistSong=PlaylistSong(playlist_id =None,song_id=song)
-#         db.session.add(new_playlistSong)
-#         db.session.commit()
-#         # flash("Song added to playlists", "success")
-
-#         # return redirect(f"/playlists/{playlist_id}")
+    return render_template("add_song_to_playlist.html", playlist=playlist, form=form, songs=form.song.choices)
